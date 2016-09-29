90c13e989136121733cac13b6994c6f668a61e71
Daniel Gonzalez <ogonzalez29@users.noreply.github.com>
2016-09-29 11:30:24 -0500

    added print to pdf feature with page break for localhost

--- print_pdf.php
+++ print_pdf.php
@@ -19,11 +19,13 @@ use mikehaertl\wkhtmlto\Pdf;
 // You can pass a filename, a HTML string, an URL or an options array to the constructor
 $pdf = new Pdf(array(
 	'page-size' => 'Letter',
-	// 'margin-top' => 20, //Change margin values to proper display. It depends on server
-	// 'margin-bottom' => 30, //Change margin values to proper display. It depends on server
+	'margin-top'    => 10,
+    'margin-right'  => 0,
+    'margin-bottom' => 10,
+    'margin-left'   => 0,
 	));
 
-$pdf->addPage('localhost/control-calidad/printcc.html');
+$pdf->addPage('localhost/inspecciones/printi.html');
 //webserver
 // $pdf->addPage('/home/servital/public_html/control-calidad/printcc.html');
 
@@ -32,33 +34,33 @@ $pdf->binary = 'C:\Archivos de programa\wkhtmltopdf\bin\wkhtmltopdf.exe';
 //webserver
 // $pdf->binary = '/home/servital/wkhtmltox/bin/wkhtmltopdf';
 
-@$search = $_SESSION['cons'];
-@$doc1 = $_POST['doc1'];
+@$search3 = $_SESSION['cons3'];
+@$doc4 = $_POST['doc4'];
 
 //get last results from database if recently submitted
-$result = mysql_query("SELECT * FROM document ORDER BY id DESC LIMIT 1")
+$result3 = mysql_query("SELECT * FROM document3 ORDER BY id DESC LIMIT 1")
 	or die(mysql_error());
 
-if (!empty($search)) {
-	$result = mysql_query("SELECT * FROM document WHERE id = '$doc1'")
+if (!empty($search3)) {
+	$result3 = mysql_query("SELECT * FROM document3 WHERE id = '$doc4'")
 		or die(mysql_error());
 
 	//If there's no information in database from search query
-	if (mysql_num_rows($result) == 0) {
+	if (mysql_num_rows($result3) == 0) {
 		die('No hay información con ese criterio de búsqueda');
 	}
 }	
 
 //loop through results of database query, displaying them in the format
-while ($row = mysql_fetch_array($result)) {
+while ($row3 = mysql_fetch_array($result3)) {
 
-	$doc1 = $row['id']+1000;
-	$license = $row['license'];
-	$day = $row['day'];
-	$month = $row['month'];
-	$year = $row['year']-2000;
+	$doc4 = $row3['id']+3000;
+	$license = $row3['license'];
+	$day = $row3['day'];
+	$month = $row3['month'];
+	$year = $row3['year']-2000;
 
-	if (!$pdf->send($doc1.'_'.$license.'_'.$day.$month.$year.'.pdf')) {
+	if (!$pdf->send($doc4.'_'.$license.'_'.$day.$month.$year.'.pdf')) {
 	    echo $pdf->getError();
 	}
 }
